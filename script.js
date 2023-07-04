d3f34ee36a512b775b85440301bb0ef3dc397add
Sebastian Linnet <sebastian.linnet@gmail.com>
2023-07-04 11:33:57 +0200

    initial design

---
+++ script.js
@@ -0,0 +1,25 @@
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+const swiper = new Swiper('.swiper', {
+    
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
