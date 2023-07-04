cee1120e09c2ac79dd197d9bb1857f50f90124ca
Sebastian Linnet <sebastian.linnet@gmail.com>
2023-07-04 13:16:58 +0200

    various stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': "#303841",
+      },
+    },
   },
   plugins: [],
 }
